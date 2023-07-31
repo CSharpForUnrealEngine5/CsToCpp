ed166499bda11459468c9d453bf934552e086108
jcoder58 <4xcoder@gmail.com>
2023-07-31 03:17:13 -0600

    Add methods

--- Compiler/GameFramework5_2/GameFramework/TMap.cs
+++ Compiler/GameFramework5_2/GameFramework/TMap.cs
@@ -1,5 +1,27 @@
 ﻿namespace GameFramework;
 
-public class TMap<K,V>
+public partial struct TMap<K,V>
 {
+    public void Empty(int ExpectedNumElements = 0) { }
+    public void Reset() { }
+    public void Shrink() { }
+    public void Compact() { }
+    public void CompactStable() { }
+    public void Reserve(int Number) { }
+    public bool IsEmpty() { return false; }
+    public int Num() { return 0; }
+    public int GetKeys(TArray<K> Keys) { return 0; }
+    public long GetAllocatedSize() { return 0; }
+    public V Add(K Key, V  Value) { return default; }
+
+    public int Remove(K Key) { return 0; }
+    public K FindKey(V Value) { return default; }
+
+    public V Find(K Key) { return default; }
+
+    public V FindOrAdd(K Key, V Value) { return default; }
+    public bool Contains(K Key) { return false; }
+
+    public void GenerateKeyArray(TArray<K> Keys) { }
+    public void GenerateValueArray(TArray<V> Values) { }
 }
--- Compiler/GameFramework5_2/GameFramework/TSet.cs
+++ Compiler/GameFramework5_2/GameFramework/TSet.cs
@@ -1,5 +1,17 @@
 ﻿namespace GameFramework;
 
-public class TSet<T>
+public partial struct TSet<T>
 {
+    public void Empty(int ExpectedNumElements = 0) { }
+    public void Reset() { }
+    public void Shrink() { }
+    public void Compact() { }
+    public void CompactStable() { }
+    public void Reserve(int Number) { }
+    public bool IsEmpty() { return false; }
+    public int Num() { return 0; }
+    public long GetAllocatedSize() { return 0; }
+
+    public void Add(T Value) { }
 }
+
