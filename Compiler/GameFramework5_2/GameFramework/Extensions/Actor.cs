28899d56718b16c48120c411228a6d33380451bd
jcoder58 <4xcoder@gmail.com>
2023-08-05 06:28:27 -0600

    Add AActor functions

--- Compiler/GameFramework5_2/GameFramework/Extensions/Actor.cs
+++ Compiler/GameFramework5_2/GameFramework/Extensions/Actor.cs
@@ -14,5 +14,21 @@ public partial class AActor : UObject
 
     public virtual void Destroy() { }
 
+    public void AddActorWorldOffset(FVector DeltaLocation) { }
+    public void AddActorWorldRotation(FRotator DeltaRotation) { }
+
+    public void AddActorWorldTransformKeepScale(FTransform DeltaTransform) { }
+
+    public bool SetActorTransform(FTransform DeltaTransform) { return false; }
+    public void AddActorLocalOffset(FVector DeltaLocation) { }
+
+    public void AddActorLocalRotation(FRotator DeltaRotation) { }
+
+    public void AddActorLocalTransform(FTransform NewTransform) { }
+
+    public void SetActorRelativeLocation(FVector NewRelativeLocation) { }
+
+    public void SetActorRelativeRotation(FRotator NewRelativeRotation) { }
+    public void SetActorRelativeTransform(FTransform NewRelativeTransform) { }
 }
 
